372d451c3ab06114974a00d4acae09736e328353
ardansisman <ardann.68@gmail.com>
2022-03-08 13:22:26 +0300

    --first commit

---
+++ src/components/Server/Server.vue
@@ -0,0 +1,11 @@
+<template>
+  <li class="list-group-item">
+    Sunucu #{{ serverData.id }}
+    <p>Durumu : {{ serverData.status }}</p>
+  </li>
+</template>
+<script>
+export default {
+  props: ["serverData"],
+};
+</script>
---
+++ src/components/Server/ServerDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-xs-12 col-sm-6" v-if="Object.keys(server).length === 0">
+    <p>Sunucu Bilgisi güncel değil!!</p>
+  </div>
+  <div class="col-xs-12 col-sm-6" v-else>
+    <p>Sunucu Bilgisi güncel değil!!</p>
+    <h3>Sunucu #{{ server.id }}</h3>
+    <h3>Sunucu Durumu :{{ server.status }}</h3>
+    <button class="btn btn-xs btn-primary" @click="changeStatus">
+      Durumu Değiştir
+    </button>
+  </div>
+</template>
+<script>
+import { eventBus } from "../../main.js";
+export default {
+  data: function () {
+    return { server: {} };
+  },
+  created: function () {
+    eventBus.$on("serverDataSend", (data) => {
+      this.server = data;
+    });
+  },
+  methods: {
+    changeStatus: function () {
+      this.server.status = "Normal";
+    },
+  },
+};
+</script>
